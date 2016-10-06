caf771698c327edfed7c0337de216383503edcb0
Zachary Drago <zacharydrago@MacBook-Pro-3.local>
2016-10-05 21:24:40 -0700

    initial

---
+++ Instagram-Grabber-master/src/Instagram.php
@@ -0,0 +1,101 @@
+<?php
+namespace Bolandish;
+
+class Instagram {
+    public static function getMediaByHashtag($hashtag = null, $count = 16, $assoc = false, $comment_count = false)
+    {
+        if ( empty($hashtag) || !is_string($hashtag) )
+        {
+            return false;
+        }
+        if($comment_count){
+            $comments = "comments.last($comment_count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
+        }else{
+            $comments = "comments {       count     }";
+        }
+        $hashtag = strtolower($hashtag);
+        $parameters = urlencode("ig_hashtag($hashtag) { media.first($count) {   count,   nodes {     caption,     code,   $comments,     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
+        $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
+        $media = json_decode(file_get_contents($url), ($assoc || $assoc == "array"));
+        if($assoc == "array")
+            $media = $media["media"]["nodes"];
+        else
+            $media = $media->media->nodes;
+        return $media;
+    }
+
+    public static function getMediaByUserID($user = null, $count = 16, $assoc = false, $comment_count = false)
+    {
+        if ( empty($user) || !(is_string($user) || is_int($user)) )
+        {
+            return false;
+        }
+        if($comment_count){
+            $comments = "comments.last($comment_count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
+        }else{
+            $comments = "comments {       count     }";
+        }
+        $parameters = urlencode("ig_user($user) { media.first($count) {   count,   nodes {     caption,     code,     $comments,     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
+        $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
+        $media = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        if($assoc == "array")
+            $media = $media["media"]["nodes"];
+        else
+            $media = $media->media->nodes;
+        return $media;
+    }
+
+    public static function getMediaAfterByUserID($user = null, $min_id, $count = 16, $assoc = false, $comment_count = false)
+    {
+        if ( empty($user) || !(is_string($user) || is_int($user)) )
+        {
+            return false;
+        }
+        if($comment_count){
+            $comments = "comments.last($comment_count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
+        }else{
+            $comments = "comments {       count     }";
+        }
+
+        $parameters = urlencode("ig_user($user) { media.after($min_id,$count) {   count,   nodes {     caption,     code,    $comments,   date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
+
+        $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
+        $media = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        if($assoc == "array")
+            $media = $media["media"]["nodes"];
+        else
+            $media = $media->media->nodes;
+
+        return $media;
+    }
+
+    public static function getCommentsByMediaShortcode($media_shortcode = null, $count = 16, $assoc = false)
+    {
+
+        $comments = "comments.last($count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
+
+        $parameters = urlencode("ig_shortcode({$media_shortcode}) { $comments }");
+        $url = "https://www.instagram.com/query/?q=$parameters&ref=media%3A%3Ashow";
+        $comments = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        if($assoc == "array")
+            $comments = $comments["comments"]["nodes"];
+        else
+            $comments = $comments->comments->nodes;
+        return $comments;
+    }
+
+    public static function getCommentsBeforeByMediaShortcode($media_shortcode = null, $max_id, $count = 16, $assoc = false)
+    {
+
+        $comments = "comments.before($max_id, $count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
+
+        $parameters = urlencode("ig_shortcode({$media_shortcode}) { $comments }");
+        $url = "https://www.instagram.com/query/?q=$parameters&ref=media%3A%3Ashow";
+        $comments = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
+        if($assoc == "array")
+            $comments = $comments["comments"]["nodes"];
+        else
+            $comments = $comments->comments->nodes;
+        return $comments;
+    }    
+}
---
+++ js/app.js
@@ -0,0 +1,109 @@
+var db = new PouchDB('runners');
+
+
+// Build Date & Time ID
+var fullDate = new Date();
+var month = fullDate.getMonth()+1;
+var twoDigitMonth = month+"";if(twoDigitMonth.length==1)	twoDigitMonth="0" +twoDigitMonth;
+var twoDigitDate = fullDate.getDate()+"";if(twoDigitDate.length==1)	twoDigitDate="0" +twoDigitDate;
+var date = new Date();
+var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+var dateCreated = twoDigitDate + "/" + twoDigitMonth + "/" + fullDate.getFullYear() + "|" + time;
+
+
+// Destroy/Reset Entire DB
+function addMiles() {
+	$('#lightbox').lightbox_me({
+		appearEffect: 'fadeIn',
+        centered: true,
+        showOverly: true,
+        overlayCSS: {background: '#2e3641', opacity: 0.9},
+        closeClick: true,
+        lightboxSpeed: 'fast',
+    });
+	
+	// var doc = {
+	//   "_id": dateCreated,
+	//   "name": "Kyle",
+	//   "entries": [
+	//     "0",
+	//   ]
+	// };
+	// db.put(doc);
+}
+
+
+// Console Log All Documents In DB
+// function showdb() {
+	db.allDocs({include_docs: true}, function(err, doc) {
+		console.log(doc.rows);
+	});
+// }
+
+
+// Show All Documents
+// function showDB() {
+	db.allDocs({include_docs: true}, function(err, doc) {
+		redrawRunners(doc.rows);
+		// index();
+		//numTodos = doc.total_rows;
+	});
+// }
+
+
+// 2) Build DB Items & Add to DOM
+function createRunnerListItem(runner) {
+
+	var tr = document.createElement('tr');
+
+    var tdname = document.createElement('td');
+    tdname.id = runner._id;
+	tdname.className = 'runner';
+	tdname.innerHTML = runner.name;
+
+	var tdmiles = document.createElement('td');
+    tdmiles.id = runner._id;
+	tdmiles.className = 'miles';
+	tdmiles.innerHTML = runner.entries;
+
+    tr.appendChild(tdname);
+    tr.appendChild(tdmiles);
+
+    return tr;
+}
+
+// 1) Build DB Items & Add to DOM
+function redrawRunners(runners) {
+	var table = document.getElementById('runner-list');
+    //table.innerHTML = '';
+    runners.forEach(function(runner) {
+    	table.appendChild(createRunnerListItem(runner.doc));
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Destroy/Reset Entire DB
+function reset() {
+	db.destroy(function (err, response) {
+		if (err) {
+			return console.log(err);
+		} else {
+			$('.listitem').remove();
+			db = new PouchDB('todos');
+		}
+	});
+}
