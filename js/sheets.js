9a5201a60fa37414610aded439955e27ddecd8ac
Zach Drago <zacharydrago@MacBook-Pro-3.local>
2016-10-08 04:25:39 -0700

    sheets

--- js/sheets.js
+++ js/sheets.js
@@ -2,7 +2,7 @@
 var request;
 
 // Bind to the submit event of our form
-$("#foo").submit(function(event){
+$("#miles").submit(function(event){
 
     // Abort any pending request
     if (request) {
